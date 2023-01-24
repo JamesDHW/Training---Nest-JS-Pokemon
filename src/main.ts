e3ece485a3b4217f121580e8b7501451f44a920d
jamesdhw <jameshaworthwheatman@gmail.com>
2023-01-24 12:25:19 +0000

    Add swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     abortOnError: false,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Pokemon')
+    .setDescription('Docs for Pokemon API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
